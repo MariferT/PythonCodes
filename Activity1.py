1d1b2f89047649ca9edc50d22d68b552f6fce8f7
Marifer Tablada <110541776+MariferT@users.noreply.github.com>
2022-08-09 12:54:36 +0800

    Add files via upload
    Basic code on printing variables (string, float, int)

---
+++ Activity1.py
@@ -0,0 +1,11 @@
+name = str("Marifer Tablada")
+math_grade = float(90.00)
+science_grade = float(87.88)
+english_grade = int(93)
+status = str("PASSED")
+
+print("Name:", name)
+print("Math Grade:", math_grade)
+print("Science Grade:", science_grade)
+print("English Grade:", english_grade)
+print("Status:", status)
